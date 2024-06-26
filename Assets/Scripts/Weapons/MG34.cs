96c71c85a7e3d5491c14bdd1f4ebe8c99b793f8a
PepperDoduk <ttokbeagi@gmail.com>
2024-06-26 21:26:13 +0900

    애니메이션 수정
    +애니메이터 수정
    +사운드 추가

---
+++ Assets/Scripts/Weapons/MG34.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG34: MonoBehaviour
+{
+
+    public Animator anim;
+    public int animNum = 0;
+
+    public int randF;
+
+    public int maxAmmo = 10;
+    public int currentAmmo;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        currentAmmo = maxAmmo;
+    }
+
+    void Update()
+    {
+        anim.SetInteger("MG34", animNum);
+
+        if (currentAmmo < 1)
+        {
+            animNum = 1;
+            
+        }
+
+        if (Input.GetMouseButtonDown(0) && currentAmmo > 0)
+        {
+            //randF = Random.Range(-3, 0);
+
+            animNum = -1;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            animNum = 0;
+        }
+    }
+
+    public void Fire()
+    {
+        
+        currentAmmo--;
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretFire);
+    }
+    public void RemoveMag()
+    {
+        
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretR1);
+    }
+    public void MagCombination()
+    {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretR2);
+    }
+    public void MagHit()
+    {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretR3);
+    }
+    public void RetractBolt()
+    {
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretR4);
+    }
+    public void Boltadvance()
+    {
+        
+        AudioManager.instance.PlaySfx(AudioManager.Sfx.BarretR5);
+    }
+
+    public void Reaload()
+    {
+        currentAmmo = maxAmmo;
+        animNum = 0;
+    }
+}
