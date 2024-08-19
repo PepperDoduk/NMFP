2bea6e90c0b7238016568a99f2a28be8b42f01e2
24기구원 <koowonqwe2@gmail.com>
2024-08-19 19:37:31 +0900

    스포너 적 스크립트 고침
    스포너는 여려 적이 소환 가능하게 적 스크립트는 문제가 있고 가독성이 좋지않은것을 바꿈

--- Assets/Scripts/K_scripts/K_EnemySpawner.cs
+++ Assets/Scripts/K_scripts/K_EnemySpawner.cs
@@ -3,24 +3,29 @@ using UnityEngine;
 
 public class K_EnemySpawner : MonoBehaviour
 {
-    public GameObject Monster;
+    public GameObject[] Monsters; 
     public float MonsterInterval = 10f;
-    public Vector3 spawnerPosition = new Vector3(-7.5f, 2.1f, -8.9f);
+    public Vector3 spawnerPosition = new Vector3(-7.5f, 2.1f, -8.9f);// let me know the approximate location where you plan to summon
     public float spawnRange = 2.0f;
 
     void Start()
     {
-        StartCoroutine(spawnEnemy(MonsterInterval, Monster));
+        StartCoroutine(spawnEnemy(MonsterInterval));
     }
 
-    IEnumerator spawnEnemy(float interval, GameObject enemy)
+    IEnumerator spawnEnemy(float interval)
     {
         while (true)
         {
             yield return new WaitForSeconds(interval);
             Vector3 spawnPosition = spawnerPosition + Random.insideUnitSphere * spawnRange;
-            spawnPosition.y = spawnerPosition.y; // y °ª °íÁ¤
-            Instantiate(enemy, spawnPosition, Quaternion.identity);
+            spawnPosition.y = spawnerPosition.y; 
+
+           
+            int randomIndex = Random.Range(0, Monsters.Length);
+            GameObject randomMonster = Monsters[randomIndex];
+
+            Instantiate(randomMonster, spawnPosition, Quaternion.identity);
         }
     }
 }
--- Assets/Scripts/K_scripts/K_Enenmy.cs
+++ Assets/Scripts/K_scripts/K_Enenmy.cs
@@ -6,32 +6,41 @@ public class K_Enenmy : MonoBehaviour
     public float lookRadius = 10f;
     public Transform target;
     public NavMeshAgent agent;
+    public int Hp;
+    private int currenthp;
+    
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-        
         GameObject player = GameObject.FindWithTag("Player");
-        
-      
-            target = player.transform;
-        
-       
+        target = player.transform;
+        currenthp = Hp;
     }
 
     void Update()
     {
-        if (target != null)
-        {
-            float distance = Vector3.Distance(target.position, transform.position);
-            agent.SetDestination(target.position);
-        }
+
+        float distance = Vector3.Distance(target.position, transform.position);
+        agent.SetDestination(target.position);
+
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+    public void TakeDamage(int damage)
+    {
+        currenthp -= damage;
+        if(currenthp<=0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
     }
 }
