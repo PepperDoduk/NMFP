f5a29136d6d0716abfb3cf76095866315a3f417d
Nevil0713 <128784519+Nevil0713@users.noreply.github.com>
2024-09-20 11:35:51 +0900

    플레이어 피격

--- Assets/Scripts/Nev/N_PlayerState.cs
+++ Assets/Scripts/Nev/N_PlayerState.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class N_PlayerState : MonoBehaviour
 {
-    [SerializeField] private int m_health;
+    [SerializeField] private int health;
 
     private void Start()
     {
-        m_health = 100;
+        health = 100;
     }
 
-    void TakeDamage(int pDamage)
+    public void TakeDamage(int pDamage)
     {
-        m_health -= pDamage;
-        if(m_health <= 0)
+        health -= pDamage;
+        if(health <= 0)
         {
             // 플레이어 사망, 게임 오버
+            Destroy(gameObject);
         }
     }
 }
